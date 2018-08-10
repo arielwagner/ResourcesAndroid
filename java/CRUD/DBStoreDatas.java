bb3f5efa5ce3be193de9c261299f4b7338c4e488
Ariel Alejandro Wagner <31016611+arielwagner@users.noreply.github.com>
2018-08-10 13:52:28 -0300

    DBStoreDatas
    Se trata de un Helper que fusiona un controlador con el gestor de base de datos para SQLite, incluyendo una Clase de Entidad de datos llamada Users, que es utilizada para estructurar los datos. Este Helper incluye un CRUD básico completo. Las listas o ítem de retorno, lo realiza mediante el uso de una clase ArrayList<?>. Esto permite enlistar u obtener los datos desde la base de datos directamente. Aquí en el ejemplo, partimos hipotéticamente de que en la base de datos, existe una tabla llamada Agenda y que posee cuatro campos. El campo Id de tipo numérico y autoincremental y el resto de los campos, son de tipo cadena o Text (String para el código de Java Android). 
    
    Observación
    Para una mejor compresión respecto a la codificación de la clase Users, en complemento a este Helper, incluyo en otro archivo, la Clase de Entidad de datos llamada Users respectivamente.

---
+++ java/CRUD/DBStoreDatas.java
@@ -0,0 +1,140 @@
+package com.cursoandroid.practica9;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+
+public class DBStoreDatas extends SQLiteOpenHelper {
+
+    private static final String DBNAME = "agendadb.s3db";
+    private SQLiteDatabase db;
+    private Context _context;
+
+    public DBStoreDatas(Context context) {
+        super(context, DBNAME, null, 1);
+        this._context = context;
+    }
+
+    public DBStoreDatas(
+            Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Crea la tabla una sola vez.
+        db.execSQL("CREATE TABLE IF NOT EXISTS Agenda (id INTEGER PRIMARY KEY NOT NULL, "
+            + "username TEXT NOT NULL, telephone TEXT NOT NULL, email TEXT NOT NULL);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // No usado.
+    }
+
+    public ArrayList<Users> getList() {
+        try {
+            ArrayList<Users> data = new ArrayList<Users>();
+            DBStoreDatas admin = new DBStoreDatas(_context, DBNAME, null, 1);
+            db = admin.getReadableDatabase();
+            String query = "SELECT * FROM Agenda ORDER By id ASC;";
+            Cursor c = db.rawQuery(query, null);
+            data.clear();
+            if(c!=null || db!= null) {
+                while(c.moveToNext()) {
+                    data.add(
+                        new Users(
+                            c.getInt(0),
+                            c.getString(1).trim(),
+                            c.getString(2).trim(),
+                            c.getString(3).trim()
+                        )
+                    );
+                }
+            }
+            db.close();
+            return data;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public ArrayList<Users> getItem(int id) {
+        try {
+            ArrayList<Users> data = new ArrayList<Users>();
+            DBStoreDatas admin = new DBStoreDatas(_context, DBNAME, null, 1);
+            db = admin.getReadableDatabase();
+            String query = String.format("SELECT * FROM Agenda WHERE id = %s;", id);
+            Cursor c = db.rawQuery(query, null);
+            data.clear();
+            if(c!=null || db!= null) {
+                while (c.moveToNext()) {
+                    data.add(
+                        new Users(
+                            c.getInt(0),
+                            c.getString(1).trim(),
+                            c.getString(2).trim(),
+                            c.getString(3).trim()
+                        )
+                    );
+                }
+            }
+            db.close();
+            return data;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public long add(Users entityclassdata) {
+        try {
+            long retStatus = 0;
+            DBStoreDatas ld = new DBStoreDatas(_context, DBNAME, null, 1);
+            db = ld.getWritableDatabase();
+            ContentValues reg = new ContentValues();
+            reg.put("username", entityclassdata.getUsername().trim());
+            reg.put("telephone", entityclassdata.getTelephone().trim());
+            reg.put("email", entityclassdata.getEmail().trim());
+            retStatus = db.insert("Agenda", null,  reg);
+            db.close();
+            return retStatus;
+        } catch (Exception ex) {
+            return 0;
+        }
+    }
+
+    public long delete(Users entityclassdata) {
+        try {
+            long retStatus = 0;
+            DBStoreDatas ld = new DBStoreDatas(_context, DBNAME, null, 1);
+            db = ld.getWritableDatabase();
+            String[] args = { String.valueOf(entityclassdata.getId()) };
+            retStatus = db.delete("Agenda", "id=?", args);
+            db.close();
+            return retStatus;
+        } catch (Exception ex) {
+            return 0;
+        }
+    }
+
+    public long update(Users entityclassdata) {
+        try {
+            long retStatus = 0;
+            DBStoreDatas ld = new DBStoreDatas(_context, DBNAME, null, 1);
+            db = ld.getWritableDatabase();
+            ContentValues reg = new ContentValues();
+            reg.put("username", entityclassdata.getUsername().trim());
+            reg.put("telephone", entityclassdata.getTelephone().trim());
+            reg.put("email", entityclassdata.getEmail().trim());
+            String[] args = { String.valueOf(entityclassdata.getId()) };
+            retStatus = db.update("Agenda", reg, "id=?", args);
+            db.close();
+            return retStatus;
+        } catch (Exception ex) {
+            return 0;
+        }
+    }
+}
